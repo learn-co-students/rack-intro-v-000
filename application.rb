470f263f4f52eaf7f273333419c5bc0a2888f842
tddycks <tadzio.smith@gmail.com>
2016-06-03 17:10:05 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Tadzio Smith and I'm ready for a nice cup of tea."
     resp.finish
   end
 
