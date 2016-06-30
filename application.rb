678fb13421d10079c392758dd151e4249c27c713
Dom McKellar <dom.mckellar@gmail.com>
2016-06-29 21:03:23 -0700

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,10 @@
 class Application
 
   def call(env)
+    my_name = "Dom"
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is #{my_name}"
     resp.finish
   end
 
 end
-
