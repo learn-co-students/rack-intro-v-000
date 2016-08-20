68cbe3d8f3de4ca94cdb6ca8a050d4c2d2861918
bmolina-nyc <bruce.d.molina@gmail.com>
2016-08-20 14:22:58 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,9 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Bruce" + "\n"
+    resp.write "I'm going to make it"  + "\n"
+    resp.write "I don't know when or how but i'll be ok. I have my family and my loving wife to support me"
     resp.finish
   end
 
