d9b6b93c8783f313e22cf4989e6a218555473e57
Elyse Klova <elyse.klova@gmail.com>
2016-10-27 01:21:06 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is CHICKA CHICKA SLIM SHADY"
     resp.finish
   end
 
 end
-
