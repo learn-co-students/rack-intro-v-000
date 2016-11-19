bdc3f0c3fd33d038f34ec7753f6d2171eaa70ac5
Ray Mulligan <rmullig2@optonline.net>
2016-11-19 23:54:15 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,8 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.write "\nMy name is Ray"
+    resp.write "Hello, my name is Ray"
     resp.finish
   end
 
