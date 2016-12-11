b482f40903cd24c2a7b6ed73cc98f7c6fe47aaa2
Chirag Dudhat <cdudhat@gmail.com>
2016-12-11 03:40:14 +0000

    Done.

--- application.rb
+++ application.rb
@@ -1,9 +1,36 @@
 class Application
-
   def call(env)
     resp = Rack::Response.new
     resp.write "Hello, my name is Chirag"
     resp.finish
   end
+
+  #Just testing data from another lab
+  #@@items = ["Apples","Carrots","Pears"]
+
+  #def call(env)
+  #  resp = Rack::Response.new
+  #  req = Rack::Request.new(env)
+
+  #  if req.path.match(/items/)
+  #    @@items.each do |item|
+  #      resp.write "#{item}\n"
+  #    end
+  #  elsif req.path.match(/search/)
+
+  #    search_term = req.params["q"]
+
+  #    if @@items.include?(search_term)
+  #      resp.write "#{search_term} is one of our items"
+  #    else
+  #      resp.write "Couldn't find #{search_term}"
+  #    end
+
+  #  else
+  #    resp.write "Path Not Found"
+  #  end
+
+  #  resp.finish
+  #end
 
 end
