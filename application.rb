0737e8ab6cbfbc1a1a42dbd117d9554f7fb0e53c
Justin Acker <justin@justinacker.com>
2017-01-17 21:00:00 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is who? My name is what? My name is (wicka wicka) Justin Acker."
     resp.finish
   end
 
 end
-
