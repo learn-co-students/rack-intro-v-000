8d4846840a6b65e92abbd384182976aa96d8d95f
Justin Acker <justin@justinacker.com>
2017-01-17 21:02:55 +0000

    Done.

--- application.rb
+++ application.rb
@@ -1,5 +1,5 @@
 class Application
-
+  #adding comments so learn submit will function
   def call(env)
     resp = Rack::Response.new
     resp.write "Hello, my name is who? My name is what? My name is (wicka wicka) Justin Acker."
--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
 require_relative "./application.rb"
-
+#adding comments so learn submit will function
 run Application.new
