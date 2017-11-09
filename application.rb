6d7e584c1eed3b7b461737401a78f45fb8ee94b8
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-09 16:35:05 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,16 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, I'm Miriam and this is my first time in the back end!"
+    resp.write "Hello, my name is"
     resp.finish
   end
 
 end
-
-<<<<<<< HEAD
-#creates a rack::response object. adds text to it and completes it with a finish method.
-# --env holds the request info
-=======
-#creates a rack::response object. adds text to it and completes it with a finish method. 
-# --env holds the request info 
->>>>>>> c3763d44cb3d61af7f9e8450f97e27639ab230f6
