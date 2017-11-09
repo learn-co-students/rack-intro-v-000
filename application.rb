94c7313a0c3dd014e18c0526018d8293daf3672c
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-09 16:31:32 +0000

    Done.

--- application.rb
+++ application.rb
@@ -8,5 +8,10 @@ class Application
 
 end
 
+<<<<<<< HEAD
 #creates a rack::response object. adds text to it and completes it with a finish method.
 # --env holds the request info
+=======
+#creates a rack::response object. adds text to it and completes it with a finish method. 
+# --env holds the request info 
+>>>>>>> c3763d44cb3d61af7f9e8450f97e27639ab230f6
