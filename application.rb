5c800c87ee1877be262d35a77e98dd245be6bdd4
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-12-02 04:17:09 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Joey Michalina Mariano."
     resp.finish
   end
 
 end
-
