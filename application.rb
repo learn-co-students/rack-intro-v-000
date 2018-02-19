3341a17e2158f3bfe07653abc034ce78ad8e4bbf
Anthony Harvey <anthonygharvey@gmail.com>
2018-02-19 03:30:56 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,9 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    name = "Anthony"
+    resp.write "Hello, my name is #{name}"
     resp.finish
   end
 
 end
-
