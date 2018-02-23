596027c30f15bd830373f118aafc4b0658a99b23
jljardon <github email address>
2018-02-22 20:10:59 -0500

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Jose L. Jardon."
     resp.finish
   end
 
 end
-
