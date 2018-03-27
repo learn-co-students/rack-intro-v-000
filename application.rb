988784b180b6f126833eccdf9c0c844a403001bc
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-03-27 14:12:03 -0700

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Andrew and this is the first HTTP response I've made!"
     resp.finish
   end
 
