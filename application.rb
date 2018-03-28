798025ad789ce9576fd2e92039480932d5112b9f
Alex McKean <amckean@me.com>
2018-03-28 17:01:58 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World My nam is Alex McKean"
+    resp.write "Hello, my name is Alex McKean"
     resp.finish
   end
 
