7acdc7c28b30f27fb8aa31f230e2c9b35756d47a
Rollin Metzger <rollin.metzger@gmail.com>
2018-03-29 18:00:58 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Slim Shady and I will stand up for gun control legislation"
     resp.finish
   end
 
 end
-
