4b0344e5c2edfeec54470a2adaabf04ccc30e7e1
Frank McCallion <fkmccallion@gmail.com>
2018-04-05 13:44:58 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,9 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    #resp.write "Hello, World"
+    resp.write "Hello, my name is Keith!"
     resp.finish
   end
 
 end
-
