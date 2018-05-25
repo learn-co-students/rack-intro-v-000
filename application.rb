2641f01d2c83066e45738b75e0f0cd1468224fd9
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-05-25 12:22:11 -0700

    Done.

--- application.rb
+++ application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     resp.write "Hello, World"
-    resp.write "Hello my name is Sonia"
+    resp.write "Hello, my name is Sonia"
     resp.finish
   end
 
