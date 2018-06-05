df3626db707d4aa44345f5659fe7e6c9bc67f1ef
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-05 19:53:30 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,9 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is temitope Elodimuor."
+    resp.write "I am now learning the rack and rails was built on top of rack"
     resp.finish
   end
 
 end
-
