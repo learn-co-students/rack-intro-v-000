4ad8fac1ea4e379bb89dd8c3c3b950e2751cc1e8
Nathan  Visak <nathan.visak@gmail.com>
2018-08-15 21:48:47 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Nathan. What's yours?"
     resp.finish
   end
 
 end
-
