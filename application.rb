3b3a716287432ca724e9adb0462f8dba35973fd4
Sherry Martin <sm674@cornell.edu>
2018-10-23 10:31:55 +0000

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,9 @@
 class Application
 
-  def call(env)
-    resp = Rack::Response.new
-    resp.write "Hello, World"
+  def call(env) #env holds all of the request info in it
+    resp = Rack::Response.new #Using the Rack::Response object, returns a response which consists of the status code, any headers, and the body.
+    resp.write "Hello, my name is"
     resp.finish
   end
 
 end
-
