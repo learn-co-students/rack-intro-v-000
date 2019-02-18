66e02aa822ebf230526022501b2c556bbf0b959a
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 01:50:26 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World, It's Dianne Phenomenal Woman Jarvis!"
+    resp.write "Hello, my name is"
     resp.finish
   end
 
