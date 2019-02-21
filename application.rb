ac4c1f1d2bcbb705a7dc399ad436a505fe25df29
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-21 16:36:59 +0000

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,10 @@
 class Application
 
-  def call(env)
+  def call(env) #=>> env holds all of the request info
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Sheena!"
     resp.finish
   end
-
+  #=> method above will run whenever there is a request received
 end
 
--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
+# sets up an HTTP web server that will receive the HTTP request
+
 require_relative "./application.rb"
 
 run Application.new
